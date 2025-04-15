232ed57bb2851212dbb29b0cdf942a2d4b373d07
Fernando <fernandodevpascoal@gmail.com>
2025-04-15 14:08:54 -0400

    config builds

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
-import dts from 'vite-plugin-dts';
+import dts from "vite-plugin-dts";
+import path from 'path';
+import url from 'url';
+
+const _dirname =
+  typeof __dirname === 'undefined'
+    ? path.dirname(url.fileURLToPath(import.meta.url))
+    : __dirname;
 
 // https://vite.dev/config/
 export default defineConfig({
@@ -9,15 +16,15 @@ export default defineConfig({
     react(),
     tailwindcss(),
     dts({
-      tsconfigPath: "tsconfig.json",
+      tsconfigPath: "tsconfig.build.json",
       outDir: "dist",
       insertTypesEntry: true,
     }),
   ],
   build: {
     lib: {
-      entry: "./src/index.ts",
-      name: "W3BlockSDK",
+      entry: path.resolve(_dirname, 'src/index.tsx'),
+      name: "w3block-new-sdk",
       formats: ["es", "cjs"],
       fileName: (format) => `w3block-sdk.${format}.js`,
     },
